4088b7ff52247022219e564dc18cd7c1c0409a99
Seth <Medeirosseth@gamil.com>
2021-11-06 15:53:58 -0700

    add models and routs with mongoose

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv")
+const mongoose = require('mongoose')
 
 
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex: true,
+}).then(console.log("connected to MongoDB"))
+.catch((err) => console.log(err));
 
 app.listen("3000", ()=>{
 
