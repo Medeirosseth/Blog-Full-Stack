2b119e059f1d1be15e14196d4190460930cac917
Seth <Medeirosseth@gamil.com>
2021-11-20 22:24:57 -0800

    add successful put/update route

--- api/Routes/posts.js
+++ api/Routes/posts.js
@@ -13,7 +13,32 @@ router.post("/",  async (req, res) => {
     res.status(500).json(err)
   }
 })
+// Update Posts
 
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+      if(post.username === req.body.username) {
+        try{
+          const updatedPost = await Post.findByIdAndUpdate(
+            req.params.id, 
+            {
+              $set: req.body,
+            },
+            { new: true }
+            );
+            res.status(200).json(updatedPost)
+        }catch (err) {
+          res.status(500).json(err)
+        }
+    } else {
+      res.status(401).json("Only you can update your post")
+    }
+  }catch(err) {
+    res.status(500).json(err)
+
+  }
+}) 
 //Delete Post
 
 router.delete("/:id",  async (req, res) => {
