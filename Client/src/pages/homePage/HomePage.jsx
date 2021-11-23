7b63e2223dfae6166a205357e7c895a091664211
Seth <Medeirosseth@gamil.com>
2021-11-22 16:56:15 -0800

    yarn upgrade

--- Client/src/pages/homePage/HomePage.jsx
+++ Client/src/pages/homePage/HomePage.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Header from '../../components/Header/Header'
 import Posts from '../../components/posts/Posts'
 import SideBar from '../../components/Siderbar/SideBar'
 import './homepage.css'
+import axios from "axios"
 
 export default function HomePage() {
+  const [posts, setPosts] = useState([])
+
+
+  useEffect(() => {
+    const fetchPosts = async ()=>{
+      const res = axios.get("/posts")
+      setPosts(res.data)
+    }
+    fetchPosts()
+  }, [])
+
   return (
     <>
       <Header/>
       <div className='homePage'> 
-        <Posts/>
+        <Posts posts={posts}/>
         <SideBar/>
       </div>
     </>
